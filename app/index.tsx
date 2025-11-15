992b95922dbe8b992e1bef610c6976027dc23063
coderghoust <tabish169000@gmail.com>
2025-11-15 23:02:51 +0500

    index screen content added

--- app/index.tsx
+++ app/index.tsx
@@ -7,9 +7,14 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        padding:20
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nostrum, aliquid alias animi magni nemo, ab veritatis sapiente vitae fugiat reprehenderit explicabo aperiam labore neque voluptatum doloremque asperiores molestiae? Assumenda!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nostrum, aliquid alias animi magni nemo, ab veritatis sapiente vitae fugiat reprehenderit explicabo aperiam labore neque voluptatum doloremque asperiores molestiae? Assumenda!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nostrum, aliquid alias animi magni nemo, ab veritatis sapiente vitae fugiat reprehenderit explicabo aperiam labore neque voluptatum doloremque asperiores molestiae? Assumenda!
+        .</Text>
     </View>
   );
 }
